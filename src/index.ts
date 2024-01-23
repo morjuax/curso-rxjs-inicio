4b33e7c138aadbf20ccd790b5ad19c73a693a143
Juan Moreno <jcarlmorenos@falabella.cl>
2024-01-22 21:48:46 -0300

    fix: index

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,4 @@ const observer: Observer<any> = {
   error: error => console.warn('error: ', error),
   complete: () => console.info('Completed [obs]')
   
-}
-
-// const obs$ = Observable.create()
-const obs$ = new Observable<string>( subs => {
-
-  subs.next('Hola')
-  subs.next('Mundo')
-
-  subs.next('Hola')
-  subs.next('Mundo')
-
-  // force error
-  // const a = undefined;
-  // a.name = 'Juan'
-
-  subs.complete();
-});
-
-// obs$.subscribe(
-//   {
-//     next: (value) => console.log('next: ', value),
-//     error: (error) => console.warn('error: ', error),
-//     complete: () => console.log('Completed')
-//   }
-// )
-
-obs$.subscribe(observer)
+};
